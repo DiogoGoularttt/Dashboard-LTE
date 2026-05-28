376a85422c3c44e08587b517668a81a3acafe251
Diogo Goulart <devdiogogoulart@gmail.com>
2026-05-28 01:16:11 -0300

    :rocket: feat: Add new layouts

---
+++ resources/views/parts/header-content.blade.php
@@ -0,0 +1,12 @@
+<div class="app-content-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <h3 class="mb-0">@yield('page-title')</h3>
+            </div>
+            <div class="col-sm-6">
+                Actions
+            </div>
+        </div>
+    </div>
+</div>
